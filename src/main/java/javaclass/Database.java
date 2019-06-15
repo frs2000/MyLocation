64794c91f69abf13a45b3ca7bd16f74d6ea821c5
frs2000 <46113874+frs2000@users.noreply.github.com>
2019-06-15 12:48:31 +0300

    new database

--- src/main/java/javaclass/Database.java
+++ src/main/java/javaclass/Database.java
@@ -1,8 +1,116 @@
 package javaclass;
 
-import org.apache.log4j.Logger;
+//http://java-online.ru/hibernate-hql.xhtml
+import entities.CoordRequest;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class Database {
-    private static final Logger log = Logger.getLogger(Database.class);
+
+    //try - if yes (== 0 - not exist), false - if no (!= 0 - exist)
+    boolean containsParameters(String requestHQL){
+        Configuration config = new Configuration().configure();
+        ServiceRegistry servRegis = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+        SessionFactory sesFact = config.buildSessionFactory(servRegis);
+        Session session = sesFact.openSession();
+        String reqHQL = requestHQL;
+        List<CoordRequest> requestList = (List<CoordRequest>) session.createQuery(reqHQL).list();
+        session.close();
+        sesFact.close();
+        return requestList.size() == 0;
+    }
+
+    boolean containsUserName(String userName){
+        String requestHQL = "from CoordRequest where userNAme = '"+userName+"'";
+        return containsParameters(requestHQL);
+    }
+
+    //specific use entered specific coordinates (100% match)
+    boolean userEnteredCoordinates(String userName, Double longitude, Double latitude){
+        String requestHQL = "from CoordRequest where userNAme = '"+userName+"' and longitude = '"+longitude+"' and latitude = '"+latitude+"'";
+        return containsParameters(requestHQL);
+    }
+
+    boolean anybodyEnteredCoordinates(Double longitude, Double latitude){
+        String requestHQL = "from CoordRequest where longitude = '"+longitude+"'and latitude = '"+latitude+"'";
+        return containsParameters(requestHQL);
+    }
+
+    List<CoordRequest> containsParametersList(String requestHQL){
+        Configuration config = new Configuration().configure();
+        ServiceRegistry servRegis = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+        SessionFactory sesFact = config.buildSessionFactory(servRegis);
+        Session session = sesFact.openSession();
+        String reqHQL = requestHQL;
+        List<CoordRequest> requestList = (List<CoordRequest>) session.createQuery(reqHQL).list();
+        session.close();
+        sesFact.close();
+        return requestList;
+    }
+
+    //for specific user
+    boolean placeNameAndTypeNull (String userName, Double longitude, Double latitude){
+        String requestHQL = "from CoordRequest where userNAme = '"+userName+"' and longitude = '"+longitude+"' and latitude = '"+latitude+"'";
+        List<CoordRequest> requestList = containsParametersList(requestHQL);
+        for(int i  = 0 ; i<requestList.size(); i++){
+            CoordRequest tempCoordRec = requestList.get(i);
+            if (tempCoordRec.getPlaceName() != null && tempCoordRec.getPlaceType() != null ){
+                return true; //we  have data and get this data from our database
+            }
+        }
+        return false;
+    }
+
+    //for any users
+    boolean placeNaATyNull (Double longitude, Double latitude){
+        String requestHQL = "from CoordRequest where longitude = '"+longitude+"' and latitude = '"+latitude+"'";
+        List<CoordRequest> requestList = containsParametersList(requestHQL);
+        for(int i  = 0 ; i<requestList.size(); i++){
+            CoordRequest tempCoordRec = requestList.get(i);
+            if (tempCoordRec.getPlaceName() != null && tempCoordRec.getPlaceType() != null ){
+                return true; //we  have data and get this data from our database
+            }
+        }
+        return false;
+    }
+
+
+    void createdRecords(CoordRequest request){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPAExample1");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        System.out.println("--------------------------------");
+        CoordRequest req = request;
+        em.persist(req);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+    }
+
+    //request just with userName
+    void addUserName(String userName){
+        CoordRequest request = new CoordRequest(userName);
+        createdRecords(request);
+    }
+
+    void addNameAndCoord(String userName, Double longitude, Double latitude){
+        CoordRequest request = new CoordRequest(userName, longitude, latitude);
+        createdRecords(request);
+    }
+
+
+
+
+
+
+
 
 }
