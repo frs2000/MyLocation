88a005dac1b9671eef8acbcdb3961eb70574d6ee
frs2000 <46113874+frs2000@users.noreply.github.com>
2019-06-15 12:47:53 +0300

    new controller

--- src/main/java/javaclass/Controller.java
+++ src/main/java/javaclass/Controller.java
@@ -1,32 +1,49 @@
 package javaclass;
 
+
 import org.apache.log4j.Logger;
 
 public class Controller {
-    private static final Logger log = Logger.getLogger(Controller.class);
-
     private String userName;
     private Double longitude ;
     private Double latitude;
+    private static final Logger log = Logger.getLogger(Controller.class);
 
     public void setUserName(String userName){
         this.userName = userName;
-        //log.info(this.userName);
     }
 
-    public void setCoordinates(Double longitude, Double latitude){
-        this.longitude = longitude;
-        this.latitude = latitude;
+    public void setCoordinates(Double latitude, Double longitude){
+        this.longitude = latitude;
+        this.latitude = longitude;
     }
 
-    public void controlLogic(){
-
+    public void controlLogic() {
+        Database database = new Database();
+        if (database.containsUserName(userName) == true){
+            database.addNameAndCoord(userName, longitude, latitude);
+        }
+        if (database.userEnteredCoordinates(userName, longitude, latitude) == false){
+            if (database.placeNameAndTypeNull(userName, longitude, latitude) == true) {
+            } else {
+                if (database.anybodyEnteredCoordinates(longitude, latitude) == false){
+                    if (database.placeNaATyNull(longitude, latitude) == true){
+                    } else {
+                        getParamFromJSON();
+                    }
+                }
+            }
+        }
     }
 
+    void getParamFromJSON(){
+        Parser parser = new Parser(longitude, latitude);
 
+        log.info(parser.recPlaceName());
+        log.info(parser.recPlaceType());
+    }
 
+    void workWithScanner(){
 
-
-
-
+    }
 }
