96d1190170559a8069cd97e2c6693af756a287b9
frs2000 <46113874+frs2000@users.noreply.github.com>
2019-06-15 12:49:34 +0300

    new main

--- src/main/java/javaclass/Main.java
+++ src/main/java/javaclass/Main.java
@@ -1,14 +1,15 @@
 package javaclass;
 
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;
 
 public class Main {
-    private static final Logger log = Logger.getLogger(Main.class);
+    //private static final Logger log = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
+
         Controller controller = new Controller();
-        controller.setUserName("nic");
-        controller.setCoordinates(-38.487467,27.564564);
-        log.info("Ok");
+        controller.setUserName("lim");
+        controller.setCoordinates(39.750307,-104.999472);
+        controller.controlLogic();
     }
 }
